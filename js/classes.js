c170ab4e2a8b39d11c942f6c26e14e6dd1d40bc2
Hector Franco Torres <tor6.franco6@hotmail.com>
2019-05-22 16:36:08 -0500

    Usado Beautify.

--- js/classes.js
+++ js/classes.js
@@ -1,5 +1,5 @@
 class Door {
-    constructor(x, y, img, msj, width, height){
+    constructor(x, y, img, msj, width, height) {
         this.x = x
         this.y = y
         this.img = img
@@ -8,30 +8,30 @@ class Door {
         this.height = height
     }
 
-    imIn(x, y){
+    imIn(x, y) {
         return x > this.x && x < this.x + this.width && y > this.y && y < this.y + this.height
     }
-    draw(){
+    draw() {
         // code here
     }
 }
 
-class Doors{
-    constructor(x, y, img, msj, width, height){
+class Doors {
+    constructor(x, y, img, msj, width, height) {
         this.doors = [new Door(x, y, img, msj, width, height)]
     }
-    add(x, y, img, msj, width, height){
+    add(x, y, img, msj, width, height) {
         this.doors.push(new Door(x, y, img, msj, width, height))
     }
-    draw(){
-        for(let i = 0; i < this.doors.length; i++){
+    draw() {
+        for (let i = 0; i < this.doors.length; i++) {
             this.doors[i].draw()
         }
     }
-    imIn(x, y){
+    imIn(x, y) {
         let result = []
-        for(let i = 0; i < this.doors.length; i++){
-            if(this.doors[i].imIn(x, y)){
+        for (let i = 0; i < this.doors.length; i++) {
+            if (this.doors[i].imIn(x, y)) {
                 result.push(this.doors[i])
             }
         }
