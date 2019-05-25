bf712c02c8ded595334b7c9a3992b9087fec76ad
Hector Franco Torres <tor6.franco6@hotmail.com>
2019-05-25 18:59:11 -0500

    Agregada clase Player

--- js/classes.js
+++ js/classes.js
@@ -39,3 +39,54 @@ class Doors {
         return result
     }
 }
+
+class Player {
+    constructor(x, y, velocity, changeImgVelocity, playerWidth, playerHeight, imgRight, imgLeft, imgRightSecond, imgLeftSecond) {
+        this.position = createVector(x, y)
+        this.velocity = velocity
+        this.changeImgVelocity = changeImgVelocity
+        this.playerWidth = playerWidth
+        this.playerHeight = playerHeight
+        this.imgRight = imgRight
+        this.imgLeft = imgLeft
+        this.imgRightSecond = imgRightSecond
+        this.imgLeftSecond = imgLeftSecond
+        this.lastImage = 'Right'
+        this.imgCounter = 0
+    }
+    move(direction) {
+        if (direction === 'right') {
+            if (this.position.x < width - this.playerWidth) {
+                this.position.add(this.velocity)
+            }
+            this.draw(direction)
+        } else {
+            if (this.position.x > 0) {
+                this.position.sub(this.velocity)
+            }
+            this.draw(direction)
+        }
+    }
+    draw(direction) {
+        if (direction) {
+            if (this.lastImage.slice(0, 1).toLowerCase() != direction.slice(0, 1)) {
+                if (direction === 'right') {
+                    this.lastImage = 'Right'
+                } else {
+                    this.lastImage = 'Left'
+                }
+                this.imgCounter = 0
+            } else {
+                if (this.imgCounter % this.changeImgVelocity === 0) {
+                    if (direction === 'right') {
+                        this.lastImage = this.lastImage === 'Right' ? 'RightSecond' : 'Right'
+                    } else {
+                        this.lastImage = this.lastImage === 'Left' ? 'LeftSecond' : 'Left'
+                    }
+                }
+            }
+            this.imgCounter += 1
+        }
+        image(this['img' + this.lastImage], this.position.x, this.position.y, this.playerWidth, this.playerHeight)
+    }
+}
