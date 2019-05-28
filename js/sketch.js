495ed6ddc9d1e4e3d9123f089b4268c525db63ba
Hector Franco Torres <tor6.franco6@hotmail.com>
2019-05-28 18:21:38 -0500

    Nivel de prueba con imágenes reales Falta posicionarlas y solucionar bugs

--- js/sketch.js
+++ js/sketch.js
@@ -1,14 +1,19 @@
-var level, img, im2, img3, img4, doorImg, buttons, buttonImg;
+var level, img, im2, img3, img4, doorImg, buttons, buttonImg, gameover, winImg, nivel1, nivel2, nivel3;
 var levels = []
 var actualLevel = 0
 
 function preload() {
-  img = loadImage('img/respiratorio.png')
-  img2 = loadImage('img/respiratorio.jpg')
-  img3 = loadImage('img/neurona.png')
-  img4 = loadImage('img/neurona.jpg')
+  img = loadImage('img/personaje.png')
+  img2 = img
+  img3 = img
+  img4 = img
   doorImg = loadImage('img/door.jpg')
   buttonImg = loadImage('img/button.png')
+  gameover = loadImage('img/gameover.png')
+  winImg = loadImage('img/victoria.png')
+  nivel1 = loadImage('img/puerta1.jpg')
+  nivel2 = loadImage('img/puerta2.jpg')
+  nivel3 = loadImage('img/inframundo.jpg')
 }
 
 function setup() {
@@ -20,24 +25,23 @@ function setup() {
   let doors = new Doors(60, 100, doorImg, 'Hector JAJD JADRI JDFF EK', 100, 100, 70, 30)
   doors.add(200, 100, doorImg, 'Pedro', 100, 100)
   doors.add(500, 100, doorImg, 'Ramon', 100, 100)
-  levels.push(new Level('¿Como te llamas?', doors, new Player(20,200, 3, 5, 60, 90, img, img3, img2, img4 ), buttons, 100, 300, 400, 40, 20))
+  levels.push(new Level(nivel1, '¿Como te llamas?', doors, new Player(20,200, 3, 5, 60, 90, img, img3, img2, img4 ), buttons, 100, 300, 400, 40, 20))
 
   doors = new Doors(500, 100, doorImg, 'catorce', 100, 100)
   doors.add(200, 100, doorImg, 'trece', 100, 100)
   doors.add(60, 100, doorImg, 'doce', 100, 100, 70, 30)
-  levels.push(new Level('¿Cuantos años?', doors, new Player(20,200, 3, 5, 60, 90, img, img3, img2, img4 ), buttons, 100, 300, 400, 40, 20))
+  levels.push(new Level(nivel2, '¿Cuantos años?', doors, new Player(20,200, 3, 5, 60, 90, img, img3, img2, img4 ), buttons, 100, 300, 400, 40, 20))
 
   doors = new Doors(200, 100, doorImg, 'Verne', 100, 100)
   doors.add(500, 100, doorImg, 'Douglas', 100, 100)
   doors.add(60, 100, doorImg, 'Ernest', 100, 100, 70, 30)
-  levels.push(new Level('¿Autor?', doors, new Player(20,200, 3, 5, 60, 90, img, img3, img2, img4 ),buttons, 100, 300, 400, 40, 20))
+  levels.push(new Level(nivel3, '¿Autor?', doors, new Player(20,200, 3, 5, 60, 90, img, img3, img2, img4 ),buttons, 100, 300, 400, 40, 20))
 
   level = levels[0]
 
 }
 
 function draw() {
-  background('yellow')
   level.draw()
 }
 
@@ -46,17 +50,23 @@ function keyPressed() {
     if(level.iWin() != undefined){
       if(level.iWin()){
         if(actualLevel+1 === levels.length){
-          window['draw'] = () => {background('green')}
+          window['draw'] = () => {
+            background('white')
+            image(winImg, width/2-200, height/2-200, 400, 400)
+          }
         } else {
           actualLevel +=1
           level = levels[actualLevel]
           draw = () => {
-            background('yellow')
             level.draw()
           }
         }
       } else {
-        window['draw'] = () => {background('red')}
+        window['draw'] = () => {
+          background('white')
+          image(gameover, width/2-200, height/2-200, 400, 400)
+
+        }
       }
     }
   }
