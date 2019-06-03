2c32593e7e4e8b6e6312cb4761769393a294f681
Hector Franco Torres <tor6.franco6@hotmail.com>
2019-06-03 13:52:45 -0500

    Preguntas verdaderas rellenadas Eliminado ordenación aleatoria: No era necesaria, Doors ya "revuelve" el array de posiciones de puertas

--- js/questions.js
+++ js/questions.js
@@ -1,22 +1,68 @@
 var questions = [
     {
-        question: '¿Llamas',
-        correctAnswer: 'Hector',
-        answers: ['Pedro', 'Ramón']
+        question: "¿Por qué se creó el metro?",
+        correctAnswer: "Para obtener una medida igual en todo lugar",
+        answers: ["Querían una medida equivalente a 4 pies", "Se querían ver superiores ante otras naciones"]
     },
     {
-        question: 'Anos?',
-        correctAnswer: 'catorce',
-        answers: ['doce', 'trece']
+        question: "¿Quienes propusieron el metro?",
+        correctAnswer: "Franceses",
+        answers: ["Ingleses", "Estadounidenses"]
     },
     {
-        question: "autor?",
-        correctAnswer: 'Jules Verne',
-        answers: ['Douglas Adams', 'Ernest Cline']
+        question: "¿Cuál era la representación física del metro?",
+        correctAnswer: "Una barra de platino",
+        answers: ["Una regla de madera de abedúl", "Una barra de alumunio"]
+    },
+    {
+        question: "¿Cuánto disminuye el nuevo kg?",
+        correctAnswer: "50 microgramos",
+        answers: ["25 microgramos", "13 picogramos", "50 picogramos"]
+    },
+    {
+        question: "¿Con qué se medirá el nuevo kilogramo?",
+        correctAnswer: "Con un electroimán",
+        answers: ["Con las manos", "Con una báscula", "Con una balanza"]
+    },
+    {
+        question: "¿En qué fecha se estableció el nuevo kilogramo?",
+        correctAnswer: "20 de Mayo del 2019",
+        answers: ["19 de Enero del 2005", "24 de Marzo del 2019"]
+    },
+    {
+        question: "¿Qué es necesario para considerar la definición de una medida?",
+        correctAnswer: "Que tenga un patrón de medida consistente",
+        answers: ["Que utilice un sistema decimal", "Que sea aceptada por todos los científicos", "Que utilice el sistema binario"]
+    },
+    {
+        question: "¿Qué significa SI?",
+        correctAnswer: "Sistema Internacional de Unidades",
+        answers: ["Sistema Industrial de Medidas", "Sistema Integral Medible", "Sistema Intencional de Unidades"]
+    },
+    {
+        question: "¿A cuánto equivale un km?",
+        correctAnswer: "1000 m",
+        answers: ["100m", "10m", "1m", "0.1m", "0.01m"]
+    },
+    {
+        question: "¿Qué significa dm?",
+        correctAnswer: "Decímetro",
+        answers: ["Decámetro", "Metro", "Dariómetro", "Dicémetro"]
+    },
+    {
+        question: "¿Qué representa el Hombre de Vitruvio?",
+        correctAnswer: "Proporciones ideales del cuerpo humano",
+        answers: ["Dibujo de una mujer desnuda", "Proporciones entre el cuerpo y el sol"]
+    },
+    {
+        question: "Según la representación del hombre de Vitruvio, ¿Cuál de las siguientes afirmaciones es cierta?",
+        correctAnswer: "Cuatro dedos hacen una palma.",
+        answers: ["Cuatro pies hacen una palma", "Dos codos hacen una palma"]
+    },
+    {
+        question: "Según la representación del hombre de Vitruvio, ¿Cuál de las siguientes afirmaciones es cierta?",
+        correctAnswer: "Seis palmas hacen un codo.",
+        answers: ["Dos dedos hacen un codo", "Ocho pies hacen un codo"]
     }
 ]
-
-for(let i = 0; i < questions.length; i++){
-    questions[i].answers.sort(() => Math.random() - 0.5)
-}
 questions.sort(() => Math.random() - 0.5)
